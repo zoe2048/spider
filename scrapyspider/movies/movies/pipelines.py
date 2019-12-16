ae0cdc60cf84acb2ce41518d3b202962aea89580
zoe <xx@xx.com>
2019-12-16 15:54:00 +0800

    添加itempipeline：处理item，删除原spider中处理item代码

--- scrapyspider/movies/movies/pipelines.py
+++ scrapyspider/movies/movies/pipelines.py
@@ -5,7 +5,22 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
+import re
+from movies.items import DoubanMovieItem, ImdbMovieItem
+
 
 class MoviesPipeline(object):
     def process_item(self, item, spider):
-        return item
+        if isinstance(item, DoubanMovieItem):
+            year_pt = re.compile(r'\d+')
+            country_pt = re.compile(r'\d+\s/\s(.*)\s/\s')
+            dirc_pt = re.compile(r'导演:\s(.*)\s\s\s主演')
+            info = item['info']
+            item['year'] = year_pt.search(info).group()
+            item['director'] = ''.join(dirc_pt.findall(info))
+            item['country'] = ''.join(country_pt.findall(info))
+            if item['country'] == '':
+                item['country'] = ''.join(re.findall(r'\s/\s(\D+)\s/\s', info))
+            return item
+        elif isinstance(item, ImdbMovieItem):
+            return item
--- scrapyspider/movies/movies/spiders/douban_movies_top250.py
+++ scrapyspider/movies/movies/spiders/douban_movies_top250.py
@@ -3,7 +3,6 @@
 from scrapy.spiders import Spider
 from movies.items import DoubanMovieItem
 from scrapy import Request
-import re
 
 
 class DoubanMovieTop250(Spider):
@@ -19,24 +18,12 @@ class DoubanMovieTop250(Spider):
     def parse(self, response):
         item = DoubanMovieItem()
         movies = response.xpath('//ol[@class="grid_view"]/li')
-        year_pt = re.compile(r'\d+')
-        country_pt = re.compile(r'\d+\s/\s(.*)\s/\s')
-        dirc_pt = re.compile(r'导演:\s(.*)\s\s\s主演')
         for movie in movies:
             item['ranking'] = movie.xpath('.//div[@class="pic"]/em/text()').extract()[0]
             item['movie_name'] = movie.xpath('.//div[@class="hd"]/a/span[1]/text()').extract()[0]
             item['score'] = movie.xpath('.//div[@class="star"]/span[@class="rating_num"]/text()').extract()[0]
             item['score_num'] = movie.xpath('.//div[@class="star"]/span[4]/text()').re(r'(\d+)人评价')[0]
-            info = ''.join(movie.xpath('.//div[@class="bd"]/p/text()').extract()).strip()
-            year = year_pt.search(info).group()
-            country = ''.join(country_pt.findall(info))
-            if country == '':
-                country = ''.join(re.findall(r'\s/\s(\D+)\s/\s', info))
-            director = ''.join(dirc_pt.findall(info))
-            item['info'] = info
-            item['year'] = year
-            item['country'] = country
-            item['director'] = director
+            item['info'] = ''.join(movie.xpath('.//div[@class="bd"]/p/text()').extract()).strip()
             yield item
 
         # 翻页
