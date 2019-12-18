781c1e0869745184450a04f864848e83dfd2fa3f
zoe <xx@xx.com>
2019-12-18 17:57:07 +0800

    添加根据配置项自动生成文件

--- scrapyspider/chart/assit.py
+++ scrapyspider/chart/assit.py
@@ -6,3 +6,24 @@ import os
 def delfile(filenm):
     if os.path.exists(filenm):
         os.remove(filenm)
+
+
+def creat_filepaths(base, name, *filenms):
+    basedir = os.path.join(base, name)
+    nms = []
+    if 'data' in base:
+        suffix = '.txt'
+        csvnm = name + '.csv'
+        nms.append(csvnm)
+    elif 'html' in base:
+        suffix = '.html'
+    if not os.path.exists(basedir):
+        os.makedirs(basedir)
+    if len(filenms) >= 1:
+        for filenm in filenms:
+            filenm = filenm + suffix
+            nms.append(filenm)
+    filepaths = [os.path.join(basedir, nm) for nm in nms]
+    return filepaths
+
+
