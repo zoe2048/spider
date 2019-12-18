e02a1f91b067765d8c99d14b77736f00bca33026
zoe <xx@xx.com>
2019-12-18 17:57:44 +0800

    抽象为对象

--- scrapyspider/chart/extractdata.py
+++ scrapyspider/chart/extractdata.py
@@ -5,65 +5,49 @@
 """
 
 import csv
-from assit import delfile
+from assit import delfile, creat_filepaths
 import setting
 
 
-doub_path = setting.doub.get("doub_path")
-doub_csv = setting.doub.get("doub_csv")
-doub_country = setting.doub.get("cfilenm")
-doub_year = setting.doub.get("yfilenm")
+class CsvProcess(object):
+    def __init__(self, infile, outfile, filed):
+        self.infile = infile
+        self.outfile = outfile
+        self.filed = filed
 
-imdb_path = setting.imdb.get("imdb_path")
-imdb_csv = setting.imdb.get("imdb_csv")
-imdb_country = setting.imdb.get("cfilenm")
-imdb_year = setting.imdb.get("yfilenm")
+    def extract_data(self):
+        with open(self.infile, newline='', encoding='utf-8-sig') as f:
+            row_first = next(f).strip()
+            title = row_first.split(',')
+            filed_index = title.index(self.filed)
+            reader = csv.reader(f)
+            for row in reader:
+                data = row[filed_index]
+                yield data
 
-
-def extract_data(infile, datatype):
-    """
-    自动获取提取csv中某字段的index，读取数据，返回该数据的生成器
-    :param infile: csv原始文件
-    :param datatype: 从csv文件要读取的数据
-    :return: 提取的数据
-    """
-    with open(infile, newline='', encoding='utf-8-sig') as f:
-        row_first = next(f).strip()
-        title = row_first.split(',')
-        data_index = title.index(datatype)
-        reader = csv.reader(f)
-        for row in reader:
-            data = row[data_index]
-            yield data
-
-
-def write_data_to_txt(infile, outfile, datatype='country'):
-    """
-    提取csv文件中某字段名称的数据，写入txt文件
-    :param infile: csv原始文件
-    :param outfile: 生成的txt文件
-    :param datatype: 生成的txt文件中数据在csv的字段名称
-    :return:
-    """
-    if 'doub' or 'imdb' in infile:
-        outdata = extract_data(infile, datatype)
-        delfile(outfile)
-    else:
-        print('检查源文件csv是否存在或命名是否ok')
-    with open(outfile, 'a+', encoding='utf-8-sig') as f:
-        for c in outdata:
-            f.write(c + '\n')
+    def write_data_to_text(self):
+        outdata = self.extract_data()
+        delfile(self.outfile)
+        with open(self.outfile, 'a+', encoding='utf-8-sig') as f:
+            for c in outdata:
+                f.write(c + '\n')
 
 
 if __name__ == '__main__':
-    # 生成country.txt
-    write_data_to_txt(doub_csv, doub_country)
-    write_data_to_txt(imdb_csv, imdb_country)
-
-    # 生成year.txt
-    write_data_to_txt(doub_csv, doub_year, 'year')
-    write_data_to_txt(imdb_csv, imdb_year, 'year')
-
+    base = setting.basepath
+    names = setting.names
+    for name in names:
+        fields = names.get(name)
+        paths = creat_filepaths(base, name, *fields)
+        if len(paths) == 1:
+            csvpath = paths[0]
+            print('如果要提取数据，缺少要从csv提取的列名称，请在setting中配置')
+        elif len(paths) > 1:
+            csvpath, *txtpaths = paths[0], paths[1:]
+            filenm_field = sorted(zip(*txtpaths, fields))
+            for element in filenm_field:
+                cp = CsvProcess(csvpath, element[0], element[1])
+                cp.write_data_to_text()
 
 
 
