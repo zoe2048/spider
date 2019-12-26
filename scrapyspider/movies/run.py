223f791e5ddf76a81e2003559c2b291988c962d8
zoe <xx@xx.com>
2019-12-26 15:06:09 +0800

    运行脚本

--- scrapyspider/movies/run.py
+++ scrapyspider/movies/run.py
@@ -2,4 +2,5 @@
 
 from scrapy import cmdline
 
-cmdline.execute("scrapy crawl imdb_movie_top250 -o test.csv".split())
+cmdline.execute("scrapy crawl douban_movie_top250 -o ../chart/data/doub/doub.csv" .split())
+cmdline.execute("scrapy crawl imdb_movie_top250 -o ../chart/data/imdb/imdb.csv" .split())
