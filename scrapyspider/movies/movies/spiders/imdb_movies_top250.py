89b8de4aaa32ec9d4056c279bcecf5bf7a22f1d9
zl <zhuerdrlg@outlook.com>
2024-01-01 15:59:23 +0800

    适应2023 imdb电影top250页面元素更改后爬取数据

--- scrapyspider/movies/movies/spiders/imdb_movies_top250.py
+++ scrapyspider/movies/movies/spiders/imdb_movies_top250.py
@@ -12,28 +12,29 @@ class ImdbMovieTop250(Spider):
     }
 
     def start_requests(self):
-        url = 'http://www.imdb.com/chart/top'
+        url = 'https://www.imdb.com/chart/top/'
         yield Request(url, headers=self.headers)
 
     def parse(self, response):
-        movies = response.xpath('//tbody/tr')
+        movies = response.xpath('//div[@data-testid="chart-layout-main-column"]//ul[@role="presentation"]/li')
         for movie in movies:
             item = ImdbMovieItem()
-            item['ranking'] = movie.xpath('.//td[@class="titleColumn"]/text()').re(r'(\d+)')[0]
-            item['movie_name'] = movie.xpath('.//td[@class="titleColumn"]/a/text()').extract()[0]
-            item['score'] = movie.xpath('.//td[@class="ratingColumn imdbRating"]/strong/text()').extract()[0]
-            item['year'] = movie.xpath('.//span[@class="secondaryInfo"]/text()').re(r'\d+')[0]
-            link = movie.xpath('.//td[@class="posterColumn"]/a/@href').extract()[0]
+            item['ranking'] = movie.xpath('.//div[@class="ipc-metadata-list-summary-item__c"]//a[contains(@href,"title")]/h3/text()').re_first(r'(^\d+)')
+            item['movie_name'] = movie.xpath('.//div[@class="ipc-metadata-list-summary-item__c"]//a[contains(@href,"title")]/h3/text()').re_first(r'^\d+.(.*)')
+            item['score'] = movie.xpath('.//div[@data-testid="ratingGroup--container"]/span/text()').get()
+            item['year_runtime_rated'] = movie.xpath('.//div[@class="sc-43986a27-7 dBkaPT cli-title-metadata"]/span/text()').getall()
+            link = movie.xpath('.//div[@class="ipc-metadata-list-summary-item__c"]//a/@href').get()
             next_url = 'https://www.imdb.com' + link
             yield Request(next_url, headers=self.headers, meta={'item': item}, callback=self.parse_detail)
 
     def parse_detail(self, response):
         item = response.meta['item']  # 获取parse()传递的item参数
-        tag = response.xpath('//li[@data-testid="storyline-genres"]//a[contains(@href,"genres")]/text()').extract()
+        tag = response.xpath('//div[@data-testid="genres"]//a[contains(@href,"genres")]/span/text()').getall()
         item['tag'] = ','.join(tag)
-        item['info'] = response.xpath('//li[@data-testid="storyline-taglines"]//li//span/text()').extract()
-        country = response.xpath('//li[@data-testid="title-details-origin"]//a[contains(@href,"country")]/text()').extract()
+        item['info'] = response.xpath('//p[@data-testid="plot"]/span[1]/text()').get()
+        country = response.xpath('//li[@data-testid="title-details-origin"]//a[contains(@href,"country")]/text()').getall()
         item['country'] = ','.join(country)
-        director = response.xpath('//section[@data-testid="title-cast"]//a[contains(@href,"dr")]/text()').extract()
+        director = response.xpath('//div[@class="sc-69e49b85-3 dIOekc"]//a[contains(@href,"dr")]/text()').extract()
         item['director'] = ','.join(director)
         yield item
+
